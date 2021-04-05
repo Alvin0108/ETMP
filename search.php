2b9019c09e4cf1bb9cc30b969c99181fe2eee18e
Alvin0108 <71317204+Alvin0108@users.noreply.github.com>
2021-04-05 22:14:01 +0800

    Add files via upload

---
+++ search.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Training Search</title>
+    </head>
+    <body>
+        
+        <form action="search.php" method="post">
+            <input type="text" name="train_search" placeholder="What are you looking for?"><br><br>
+            <input type="submit" name="search" value="Search"><br><br>
+            <table>
+                <tr>
+                    <th>Training Name</th>
+                    <th>Information</th>
+                </tr>
+            </table>
+        </form>       
+    </body>
+</html>
