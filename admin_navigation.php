98a13b59f6ff9364c4652b3dbb61b6ae6f3880da
Alvin <ac988763@gmail.com>
2021-05-04 20:43:24 +0800

    implement navigation for admin

---
+++ admin_navigation.php
@@ -0,0 +1,23 @@
+<!-- ETMP logo -->	
+<img src="image/logo.png" alt="logo" class ="logo" width=10% height=auto>
+	
+	
+<nav class="topnav">
+	<ul class="nav-links">
+		<li>
+			<a class="nav-link" href="admin_dashboard.php">Home</a>
+		</li>
+		<li>
+			<a class="nav-link" href="admin_training_record.php">Training Record</a>
+		</li>
+		<li>
+			<a class="nav-link" href="admin_user_record.php">User Record</a>
+		</li>
+		<li>
+			<a class="nav-link" href="admin_venue_record.php">Venue Record</a>
+		</li>
+		<li>
+			<a class="nav-link" href="logout.php">Log out</a>
+		</li>
+	</ul>
+</nav>
