23897c3f3c1ca9c8972c92754055edc2f0951bd1
Alvin <ac988763@gmail.com>
2021-05-09 15:08:21 +0800

    update minor change

--- profile.php
+++ profile.php
@@ -30,27 +30,38 @@ $user_name = $_SESSION["user_name"];
 		$query = "SELECT * FROM users WHERE user_email='$user_email'";
 		$results= mysqli_query($conn, $query);
 		$row = mysqli_fetch_assoc($results);
+		
+		$sql = "SELECT count(training_id) AS total FROM registration WHERE user_id='$user_ID'";
+		$result = mysqli_query($conn,$sql);
+		$num = mysqli_fetch_assoc($result);
+		$total = $num['total'];
+		
 		// Assign the Session variable so the page it direct can recieve the info
 		$desc = $row["description"];
 		$gender = $row["gender"];
-	?>
-	<!-- registration form -->
-	<form action="registration.php" method="post">
-		<div class="center">
-			<h1>Personal Information</h1>
-			<hr>
 		
-			<br><label for="name"><b>Name</b></label>
-			<input type="name" name="name" id="name" value=<?php echo $user_name ?> disabled><br>
+	?>
+	<form>
+		<div class="center">
+			<h2>Profile</h2>
+			<hr>
+			<fieldset>
+			<legend>Personal Information</legend>
+			<br><label><b>Name</b></label>
+			<input type="text" name="name" id="name" value=<?php echo $user_name ?> disabled><br/><br/>
 	
-			<label for="email"><b>Email</b></label>
-			<input type="email" name="email" id="email" value=<?php echo $user_email ?> disabled><br>
+			<label><b>Email</b></label>
+			<input type="text" name="email" id="email" value=<?php echo $user_email ?> disabled><br/><br/>
 
-			<label for="gender"><b>Gender</b></label>
-			<input type="gender" name="gender" id="gender" value=<?php echo $gender ?> disabled><br>
+			<label><b>Gender</b></label>
+			<input type="text" name="gender" id="gender" value=<?php echo $gender ?> disabled><br/><br/>
 			
-			<label for="desc"><b>About</b></label>
-			<input type="desc" name="desc" id="desc" value=<?php echo $desc ?> disabled><br>
+			<label><b>About</b></label>
+			<input type="text" name="desc" id="desc" value=<?php echo $desc ?> disabled><br/><br/>
+			
+			<label><b>Total Training</b></label>
+			<input type="text" name="num" id="num" value=<?php echo $total ?> disabled><br/><br/>
+			</fieldset>
 
 			<br>
 			<hr>
@@ -59,5 +70,6 @@ $user_name = $_SESSION["user_name"];
 	<div class="center">
 		<a href="account.php" class="tab">Edit Account</a>
 		<a href="registered_training.php" class="tab">View Registered Training</a>
+	</div><br/><br/>
 	</body>
 </html>
