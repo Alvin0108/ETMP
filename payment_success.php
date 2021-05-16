8cf5e5a5ee643c37ac059e0d14ff036b8506a1d7
Jcjx10 <101214763@students.swinburne.edu.my>
2021-05-16 16:55:06 +0800

    add payment_success

---
+++ payment_success.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title> Online Banking </title>
+	<meta charset ="utf-8">
+	<meta name="author" content="Chen Jun Xue">
+	<meta name="descrtiption" content="ETMP Online Banking">
+	<meta name="keywords" content="ETMP, online banking">
+	<link rel="stylesheet" href="style/style.css">
+</head>
+	<header>
+	<!--navigation-->
+	<?php include "navigation.php";?>
+	</header>
+    <body>   
+			<br/><br/><br/>
+		<div class="final_confirm">
+			
+			<h2>Payment Success</h2>
+			<a href="registered_training.php"class="return"><i class="fa fa-hand-o-right"> Return to training registered page </i></a><br/>
+		</div>
+		<br/><br/><br/>
+		<hr/>
+    </body>
+</html>
